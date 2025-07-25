9fb3b9eb0454f56da55f054b53f55eac907d4566
callmesophiii <callmesophiii@gmail.com>
2025-07-24 20:22:03 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ import cors from "cors";
 import db from "./config/connection.js";
 import usersRouter from './routes/users.js'
 import postsRouter from './routes/posts.js'
+
 dotenv.config();
+
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 const PROD_URL = process.env.PROD_URL;
 
 const whitelist = ["http://localhost:3000", PROD_URL];
